bde7abea399e70bf79cc02b8c00b66fe152b139c
akhmanchi <akhilm1995@gmail.com>
2020-02-25 21:57:13 -0500

    added pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 // Here's an example of what a sample page looks like.
 // See more: https://nextjs.org/learn/basics/getting-started/first-page
 
-export default () => (
-  <div>
-    <p>Hello DSC!</p>
-  </div>
-)
+// export default () => (
+//   <div>
+//     <p>Hello DSC!</p>
+//   </div>
+// )
+
+import Link from 'next/link';
+
+export default function Index() {
+  return (
+    <div>
+      <Link href = "/pages">
+        <a>About page</a>
+      </Link>
+      <p>Hello Next.js</p>
+      <p>
+        This is another one.
+      </p>
+    </div>
+  );
+}
